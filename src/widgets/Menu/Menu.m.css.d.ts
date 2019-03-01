485362f78ee738619f8f261f8e193f77be012ff3
Anthony Gubler <agubler@sitepen.com>
2019-03-01 10:30:16 +0000

    add select

--- src/widgets/Menu/Menu.m.css.d.ts
+++ src/widgets/Menu/Menu.m.css.d.ts
@@ -1,3 +1,6 @@
 export const root: string;
 export const link: string;
 export const selected: string;
+export const locale: string;
+export const container: string;
+export const wrapper: string;
--- src/widgets/Menu/Menu.ts
+++ src/widgets/Menu/Menu.ts
@@ -1,13 +1,22 @@
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import { w } from '@dojo/framework/widget-core/d';
+import { v, w } from '@dojo/framework/widget-core/d';
 import Link from '@dojo/framework/routing/ActiveLink';
 import Toolbar from '@dojo/widgets/toolbar';
+import Select from '@dojo/widgets/select';
 
 import * as css from './Menu.m.css';
 
 export default class Menu extends WidgetBase {
 	protected render() {
 		return w(Toolbar, { heading: 'My Dojo App!', collapseWidth: 600 }, [
+			v('div', { classes: [ css.container ] }, [
+				v('div', { classes: [ css.wrapper ] }, [
+					v('div', { classes: [ css.locale ] }, [
+						w(Select, {
+							options: [ 'en', 'de', 'fr' ]
+						})
+					])
+				]),
 			w(
 				Link,
 				{
@@ -35,6 +44,7 @@ export default class Menu extends WidgetBase {
 				},
 				['Profile']
 			)
-		]);
+			])
+		])
 	}
 }
